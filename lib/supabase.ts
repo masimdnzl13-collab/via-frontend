75cab641ddd038a7714c8887fd31ddd1b973a2d7
masimdnzl13-collab <m.asimdnzl13@gmail.com>
2026-04-30 20:27:00 +0300

    sahis dashboard eklendi

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://kvpyzuaoanuzpihxbvzs.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt2cHl6dWFvYW51enBpaHhidnpzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcyNzY1NzAsImV4cCI6MjA5Mjg1MjU3MH0.7fv8WAKTu_IEiA7HiWRT1KGSXG7mkpfTJJ-IM3hXzqU'
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+export const supabase = createClient(supabaseUrl, supabaseKey, {
   auth: {
     persistSession: true, // Kullanıcı tarayıcıyı kapatsa da oturumu açık tutar
     autoRefreshToken: true, // Oturum süresi bitince otomatik yeniler
